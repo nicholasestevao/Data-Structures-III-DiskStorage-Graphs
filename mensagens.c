4d4e5eb390f94816ce4909dcf963f3f2742ae215
nicholasestevao <nicholas.estevao@usp.br>
2022-09-14 11:25:54 -0300

    headers

---
+++ mensagens.c
@@ -0,0 +1,10 @@
+#include "mensagens.h"
+
+void msg_erro_Arq_Inexistente(){
+    printf("Falha no processamento do arquivo");
+}
+
+void msg_erro_RRN_Invalido(){
+    printf("Nao foi possivel ler o arquivo");
+}
+
