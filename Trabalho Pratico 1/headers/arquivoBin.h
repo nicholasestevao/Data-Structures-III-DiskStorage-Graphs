4155ae06ea328ba6fb5633120e543a0dcbe13d4d
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-10-01 16:37:18 -0300

    update

--- Trabalho Pratico 1/headers/arquivoBin.h
+++ Trabalho Pratico 1/headers/arquivoBin.h
@@ -2,11 +2,14 @@
 #define __ARQUIVOS_BIN_C__
 
 #include <stdlib.h>
+#include "arquivoCsv.h"
 #include "mensagens.h"
+#include "registroCabecalho.h"
+#include "registroDados.h"
 
 FILE * abrirLeitura_bin(char * nome_arquivo);
 FILE * abrirEscrita_bin(char * nome_arquivo);
 
-
+int createTable(FILE* arquivobin, FILE* arquivocsv);
 
 #endif
--- Trabalho Pratico 1/src/arquivoBin.c
+++ Trabalho Pratico 1/src/arquivoBin.c
@@ -16,3 +16,23 @@ FILE * abrirEscrita_bin(char * nome_arquivo){
     return fopen(nome_arquivo, "wb");
 }
 
+int createTable(FILE* arquivobin, FILE* arquivocsv) {
+    if(arquivobin == NULL || arquivocsv == NULL) {
+        return 1;
+    }
+
+    RegistroCabecalho *rCabecalho;
+    RegistroDados *rDados;
+
+    alocaRegistrosCabecalho(rCabecalho);
+    alocaRegistrosDados(rDados, 1);
+
+    char *buf = malloc(sizeof(char)*255);
+    fgets(buf, 255, arquivocsv);
+    free(buf);
+
+    while(!feof(arquivocsv)) {
+        fscanf(arquivocsv, "%c,%s,%s,%c%c,%d,%c,%d", rDados->idConecta, rDados->nomePoPs, rDados->nomePais, rDados->siglaPais[0], rDados->siglaPais[1], )
+    }
+
+}
