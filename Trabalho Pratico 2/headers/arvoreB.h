fca90e169db91e60e246cefc4bea3ed40ba6154c
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-10-29 15:29:04 -0300

    BinarioArvB

--- Trabalho Pratico 2/headers/arvoreB.h
+++ Trabalho Pratico 2/headers/arvoreB.h
@@ -8,8 +8,81 @@
 #include "cabecalhoArvB.h"
 #include "noArvB.h"
 
+/**
+ * @def Tamanho da pagina de disco do arquivo da arvore-B.
+ */
+#define TAM_PAGINA_DISCO_ARV_B 65
+
+//=====================================================================
+
+/**
+ * Abre arquivo da arvore-B para leitura. 
+ * Nao atualiza o status.
+ * 
+ * @param nome_arquivo Nome do arquivo.
+ * @return Retorna um ponteiro de arquivo em modo de leitura.
+ * Se o arquivo estiver inconsistente na hora de abertura retorna NULL.
+ */
+FILE* abrirLeitura_ArvB(char * nome_arquivo);
+
+/**
+ * Abre arquivo da arvore-B para escrita. 
+ * Atualiza o status do arquivo para (1) -> Inconsistente.
+ * 
+ * @param nome_arquivo Nome do arquivo.
+ * 
+ * @return Retorna um ponteiro de arquivo em modo de leitrua + escrita.
+ * Se o arquivo estiver inconsistente na hora de abertura retorna NULL.
+ */
+FILE* abrirEscrita_ArvB(char * nome_arquivo);
+
+/**
+ * Fecha arquivo binario atualizando status
+ * Necessario usar essa funcao apenas quando o arquivo foi aberto 
+ * para escrita.
+ * 
+ * @param arquivo_ArvB Ponteiro para arquivo da arvore-B.
+ */
+void fecharArquivo_ArvB(FILE * arquivo_ArvB);
+
+/**
+ * Grava registro de cabecalho no arquivo da arvore-B.
+ * 
+ * @param arquivoArvB Ponteiro para arquivo da arcore-B.
+ * @param cabecalho Ponteiro para cabecalho a ser 
+ * escrito no aquivo da arvore-B.  
+ */
+void escreverCabecalhoArquivoAvrB(FILE *arquivoArvB, cabecalhoArvB *cabecalho); 
+
+/**
+ * Escreve noh no arquivo da arvore-B.
+ * 
+ * @param arquivoArvB Ponteiro para arquivo da arcore-B.
+ * @param cabecalho Ponteiro para cabecalho a ser 
+ * escrito no aquivo da arvore-B. 
+ * @param no Ponteiro para no a ser 
+ * escrito no arquivo da arvore-B.
+ */
+void escreveNoArvB(FILE *arquivoArvB,  cabecalhoArvB *cabecalho, noArvB *no);
+
+/**
+ * Le registro de cabecalho do arquivo da arvore-B.
+ * 
+ * @param arquivoArvB Nome do arquivo.
+ */
 cabecalhoArvB* lecabecalhoArvB(FILE *arquivoArvB);
+
+/**
+ * Le registro de dados do arquivo da arvore-B por RRN
+ * 
+ * @param arquivoArvB Nome do arquivo.
+ * @param RRN Numero Relativo do Registro.
+ * 
+ * @return Retorna um ponteiro para o dado do RRN.
+ */ 
 noArvB* leNoArvB_RRN(FILE *arquivoArvB, int RRN);
+
+
 int buscaArvoreB(FILE *arquivoArvB);
 
 
--- Trabalho Pratico 2/headers/noArvB.h
+++ Trabalho Pratico 2/headers/noArvB.h
@@ -6,6 +6,28 @@
 #include <string.h>
 #include "funcoesFornecidas.h"
 
+/**
+ * 
+ * @struct noArvB 
+ * 
+ * @var folha - 
+ * @var nroChavesNo - 
+ * @var alturaNo - 
+ * @var RRNdoNo - 
+ * @var P1 -
+ * @var C1 -
+ * @var PR1 -
+ * @var P2 -
+ * @var C2 -
+ * @var PR2 -
+ * @var P3 -
+ * @var C3 -
+ * @var PR3 -
+ * @var P4 -
+ * @var C4 -
+ * @var PR4 -
+ * @var P5 - 
+ */
 typedef struct {
     char* folha;
     int* nroChavesNo;
@@ -26,8 +48,31 @@ typedef struct {
     int* P5;
 }noArvB;
 
+
+/**
+ * Aloca memoria para todos os ponteiros do RegistroDados.
+ * 
+ * @param registros vetor de RegistroDados.
+ * @param numReg quantidade de itens do vetor de RegistroDados
+ */
 void alocaNoArvB(noArvB ** registros, int numReg);
 
+/**
+ * Desaloca memoria de todos os ponteiros do RegistroDados.
+ * 
+ * @param registros vetor de RegistroDados.
+ * @param numReg quantidade de itens do vetor de RegistroDados.
+ */
 void desalocaNoArvB(noArvB ** registros, int numReg);
+
+
+/**
+ * Insere uma chave de forma ordenada em um no da arvore-B.
+ * 
+ * @param  no No onde sera inserido a chave de forma ordenada.
+ * @param Cn Chave a ser inserida.
+ * @param PRn RRN da chave (Cn) no arquivo de dados.
+ */
+void insereChaveOrdenadaNoArvB(noArvB *no, int Cn, int PRn);
 
 #endif
