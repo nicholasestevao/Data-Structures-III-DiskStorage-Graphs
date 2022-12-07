491a3a1d4c0df2302a30d3f6d023ff223c6bb3fa
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-12-07 15:22:09 -0300

    voltando o main.cpp

--- Trabalho Pratico 3/src/main.cpp
+++ Trabalho Pratico 3/src/main.cpp
@@ -1,17 +1,70 @@
 #include <iostream>
-#include "../headers/aresta.h"
 #include "../headers/arquivoBin.h"
 #include "../headers/funcionalidades.h"
 #include "../headers/funcoesFornecidas.h"
-#include "../headers/grafo.h"
-#include "../headers/vertice.h"
-
-using std::cin;
-using std::cout;
-using std::endl;
 
 int main(int argC, char *argV[]){
-    funcionalidade11CriarGrafo("binario.bin");
+    int opcao;
+    char *nome_arquivo = new char[20];
+    
+    cin >> opcao >> nome_arquivo;
+
+    switch (opcao)
+    {
+        case 1:
+            //Create table (1) - Isaac e Nicholas
+            funcionalidade1CreateTable(nome_arquivo);
+            break;
+    
+        case 2:
+            //Select (2) - Nicholas
+            funcionalidade2Select(nome_arquivo);
+            break;
+        
+        case 3:
+            //Select where (3) - Isaac
+            funcionalidade3SelectWhere(nome_arquivo);
+            break;
+        
+        case 4:
+            //Remocao logica (4) - Isaac
+            funcionalidade4Remove(nome_arquivo);
+            break;
+        
+        case 5:
+            //Insercao (5) - Nicholas
+            funcionalidade5Insert(nome_arquivo);
+            break;
+
+        case 6:
+            //Compactacao (6) - Nicholas
+            funcionalidade6Compactacao(nome_arquivo);
+            break;
+
+        case 7:
+            //CreateIndex (7) - Nicholas
+            funcionalidade7CreateIndex(nome_arquivo);
+            break;
+            
+        case 8:
+            //Select Where (8) - Isaac
+            funcionalidade8SelectWhere(nome_arquivo);
+            break;
+            
+        case 9:
+            //se
+            funcionalidade9InsertArvB(nome_arquivo);
+            break;
+
+        case 10:
+            funcionalidade10Juncao(nome_arquivo);
+            break;
+        case 11:
+            funcionalidade11CriarGrafo(nome_arquivo);
+            break;    
+    }
+
+    delete[] nome_arquivo;
     return 0;
 }   
 
